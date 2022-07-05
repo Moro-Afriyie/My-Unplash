72ac77679672dee540b736aea4f4edd34ad3de9b
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-05 11:03:42 +0000

    add the fake data for building the page

---
+++ client/src/static/data.ts
@@ -0,0 +1,57 @@
+export const images = [
+	{
+		id: 1,
+		imageUrl: 'https://source.unsplash.com/VWcPlbHglYc',
+		label: 'Image 1',
+	},
+	{
+		id: 2,
+		imageUrl: 'https://source.unsplash.com/e6FMMambeO4',
+		label: 'Image 2',
+	},
+	{
+		id: 3,
+		imageUrl: 'https://source.unsplash.com/klCiPmzUw0Y',
+		label: 'Image 3',
+	},
+	{
+		id: 4,
+		imageUrl: 'https://source.unsplash.com/O0N9MF--hK4',
+		label: 'Image 4',
+	},
+	{
+		id: 5,
+		imageUrl: 'https://source.unsplash.com/FV3GConVSss',
+		label: 'Image 5',
+	},
+	{
+		id: 6,
+		imageUrl: 'https://source.unsplash.com/0ESjL-Nw22Y',
+		label: 'Image 6',
+	},
+	{
+		id: '7',
+		imageUrl: 'https://source.unsplash.com/KTVn62x6fFw',
+		label: 'Image 7',
+	},
+	{
+		id: '8',
+		imageUrl: 'https://source.unsplash.com/VSeVhmW4_JQ',
+		label: 'Image 8',
+	},
+	{
+		id: '9',
+		imageUrl: 'https://source.unsplash.com/07aFaTf24Kg',
+		label: 'Image 9',
+	},
+	{
+		id: '10',
+		imageUrl: 'https://source.unsplash.com/DqyYTM7pR2o',
+		label: 'Image 10',
+	},
+	{
+		id: '11',
+		imageUrl: 'https://source.unsplash.com/IdNOTjPeHrE',
+		label: 'Image 11',
+	},
+];
