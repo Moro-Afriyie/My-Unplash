b39753b1528bb6f97837de7290f43c689e74e69d
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-05 11:22:15 +0000

    install and configure eslint and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
+// eslint-disable-next-line require-jsdoc
 function App() {
-	return (
-		<div className="App">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
