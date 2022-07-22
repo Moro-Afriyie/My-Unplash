a7e8675d3af251498b09a16d892c0e69db48d221
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-22 16:51:17 +0000

    update the scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,8 +28,6 @@ AppDataSource.initialize()
 		});
 
 		const port = process.env.PORT || 3000;
-		// setup express app here
-		// ...
 
 		const server = new http.Server(app);
 		server.listen(port);
@@ -41,12 +39,13 @@ AppDataSource.initialize()
 		// start express server
 
 		// insert new photos for test
-		// await AppDataSource.manager.save(
-		// 	AppDataSource.manager.create(Photo, {
-		// 		label: 'Photo 1',
-		// 		imageUrl:
-		// 			'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-		// 	})
-		// );
+		// images.forEach(async (image) => {
+		// 	await AppDataSource.manager.save(
+		// 		AppDataSource.manager.create(Photo, {
+		// 			label: image.label,
+		// 			imageUrl: image.imageUrl,
+		// 		})
+		// 	);
+		// });
 	})
 	.catch((error) => console.log(error));
