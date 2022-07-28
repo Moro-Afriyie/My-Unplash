b377897bbd8fef4198588a6d422b19c83034ef7f
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-28 13:57:50 +0000

    create the routes for the photos

--- server/src/api/photos.ts
+++ server/src/api/photos.ts
@@ -1,14 +1,46 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { AppDataSource } from '../data-source';
 import { Photo } from '../entity/Photo';
 
 const router = Router();
+const photoRepository = AppDataSource.getRepository(Photo);
 
-router.get('/', async (req, res) => {
-	const photoRepository = AppDataSource.getRepository(Photo);
+// get all photos
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const photos = await photoRepository.find();
 		res.status(200).json({ data: photos });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+// get photo by  photo id
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const photo = await photoRepository.findOneBy({ id: req.params.id });
+		res.status(200).json({ data: photo });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+// create new photo
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const photo = await photoRepository.save(req.body);
+		res.status(201).json({ data: photo });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+// delete photo
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let photoToRemove = await photoRepository.findOneBy({ id: req.params.id });
+		await photoRepository.remove(photoToRemove);
+		res.status(204);
 	} catch (error) {
 		console.log(error);
 	}
--- server/src/index.ts
+++ server/src/index.ts
@@ -14,20 +14,6 @@ AppDataSource.initialize()
 		const app = express();
 		app.use(bodyParser.json());
 
-		// register express routes from defined application routes
-		// Routes.forEach((route) => {
-		// 	(app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-		// 		const result = new (route.controller as any)()[route.action](req, res, next);
-		// 		if (result instanceof Promise) {
-		// 			result.then((result) =>
-		// 				result !== null && result !== undefined ? res.send(result) : undefined
-		// 			);
-		// 		} else if (result !== null && result !== undefined) {
-		// 			res.json(result);
-		// 		}
-		// 	});
-		// });
-
 		// initialize api routes
 		createAPI(app);
 
