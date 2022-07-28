904e3d5a9e722656a8b07ab8c6b5fb5fa6036be1
Moro-Afriyie <ceremoinc99@gmail.com>
2022-07-28 13:37:32 +0000

    add some comments

--- server/src/controller/PhotoController.ts
+++ server/src/controller/PhotoController.ts
@@ -5,18 +5,22 @@ import { NextFunction, Request, Response } from 'express';
 export class PhotoController {
 	private photoRepository = AppDataSource.getRepository(Photo);
 
+	// get all photos
 	async all(request: Request, response: Response, next: NextFunction) {
 		return this.photoRepository.find();
 	}
 
+	// get all photos by photo id
 	async one(request: Request, response: Response, next: NextFunction) {
 		return this.photoRepository.findOneBy({ id: request.params.id });
 	}
 
+	// create new photo
 	async save(request: Request, response: Response, next: NextFunction) {
 		return this.photoRepository.save(request.body);
 	}
 
+	// remove photo
 	async remove(request: Request, response: Response, next: NextFunction) {
 		let userToRemove = await this.photoRepository.findOneBy({ id: request.params.id });
 		await this.photoRepository.remove(userToRemove);
