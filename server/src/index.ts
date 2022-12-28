ed735d36cf83a73165c95e33a968cd77ab60e0d2
Moro-Afriyie <ceremoinc99@gmail.com>
2022-12-28 23:53:37 +0000

    feat:add typescript to the dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
+import { Photo } from './entity/Photo';
 import { AppDataSource } from './data-source';
 import * as http from 'http';
 import app from './app';
+import { images } from './fakeData';
 
 AppDataSource.initialize()
 	.then(async () => {
@@ -8,6 +10,15 @@ AppDataSource.initialize()
 
 		const server = new http.Server(app);
 		server.listen(port);
+
+		// images.slice(15, 20).forEach(async (item) => {
+		// 	await AppDataSource.manager.save(
+		// 		AppDataSource.manager.create(Photo, {
+		// 			imageUrl: item.imageUrl,
+		// 			label: item.label,
+		// 		})
+		// 	);
+		// });
 
 		server.on('listening', () => {
 			console.log(`My unsplash API running in ${process.env.NODE_ENV} on port ${port} 🚀🚀🚀🚀`);
